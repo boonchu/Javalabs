493c53195ced75138e22de1a8cceefb5851373af
Boonchu Ngampairoijpibul <bigchoo@gmail.com>
2016-02-01 08:49:57 -0800

    added nano time benchmark

--- data-structures-optimizing-performance/TestSorted.java
+++ data-structures-optimizing-performance/TestSorted.java
@@ -1,3 +1,5 @@
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
 // TestSorted.java
 // :
 // :
@@ -9,6 +11,7 @@ public class TestSorted
   // swap it with element in position i
   public static void selectionSorted(int[] vals)
   {
+    final long start = System.nanoTime();
 
     int indexMin;
     // O(n) * O(n-i)
@@ -29,6 +32,9 @@ public class TestSorted
         vals[indexMin] = vals[i];
         vals[i] = temp;
     }
+   
+    // System.out.println("Benchmark time is " + (long) NANOSECONDS.toSeconds((long) System.nanoTime() - start) + " seconds");
+    System.out.format("Benchmark time is %f seconds\n", ((double) System.nanoTime() - start) / 1000000000);
 
   }
 
@@ -37,6 +43,7 @@ public class TestSorted
   // worst case : O(n^2) when array is in reverse order
   public static void insertionSorted(int[] vals)
   {
+    long start = System.nanoTime();
     
     int currentIndex;
     for (int i=0; i <vals.length; i++) {
@@ -51,6 +58,8 @@ public class TestSorted
             currentIndex = currentIndex - 1; 
         }
     }
+    // System.out.println("Benchmark time is " + NANOSECONDS.toSeconds(System.nanoTime() - start) + " seconds");
+    System.out.format("Benchmark time is %f seconds\n", ((double) System.nanoTime() - start) / 1000000000);
 
   }
 
@@ -68,12 +77,14 @@ public class TestSorted
   public static void main (String[] args) 
   {
 
-    int[] vals = new int[] {1,2,5,3};
+    int[] vals = new int[] {99,8,10,4,6,9,15,24,22,1,11,2,16,5,3,78,100};
+    System.out.println("Selection Sorted Test O(n^2)");
     println(vals);
     selectionSorted(vals);
     println(vals);
 
-    vals = new int[] {3,10,5,0};
+    vals = new int[] {99,8,10,4,6,9,15,24,22,1,11,2,16,5,3,78,100};
+    System.out.println("Inserion Sorted Test O(n) -> O(n^2)");
     println(vals);
     insertionSorted(vals);
     println(vals);
